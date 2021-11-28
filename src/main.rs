222779abd4ef8299bd7770ec4db8d4c30e85fb3a
me <me@me.com>
2021-11-28 09:33:28 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,41 @@ mod maze;
 
 fn main() {
     let (mut rl, ref thread) = raylib::init()
-        .size(750, 650)
+        .size(800, 600)
         .title("Maze using Depth-First Search")
         .build();
 
-    let mut maze = maze::Maze::new(71, 71);
+    let mut maze = maze::Maze::new(20, 20);
     let w = rl.get_screen_width() / maze.width as i32;
     let h = rl.get_screen_height() / maze.height as i32;
 
-    while !rl.window_should_close() {        
+    while !rl.window_should_close() {    
         maze.generate_next();
 
+        if maze.active_position == (0, 0) {
+            rl.set_target_fps(10);
+        }
+        else {
+            rl.set_target_fps(500);
+        }
+        
         if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
-            maze = maze::Maze::new(71, 71);
+            maze = maze::Maze::new(20, 20);
         }
 
         let mut d = rl.begin_drawing(thread);
         d.clear_background(Color::BLACK);
 
         for cell in maze.iter() {
-            d.draw_rectangle(
-                cell.x * w,
-                cell.y * h,
-                w,
-                h,
-                match cell.cell_type {
-                    maze::CellType::Wall => Color::BLACK,
-                    maze::CellType::Path => Color::GREEN,
-                },
-            );
+            if cell.visited {
+                d.draw_rectangle(
+                    cell.x * w,
+                    cell.y * h,
+                    w,
+                    h,
+                    Color::WHITE,
+                );
+            }
         }
 
         d.draw_rectangle(
@@ -43,7 +49,37 @@ fn main() {
             maze.active_position.1 * h,
             w,
             h,
+            Color::RED,
+        );
+
+        d.draw_rectangle(
+            (maze.width - 1) * w,
+            (maze.height - 1) * h,
+            w,
+            h,
             Color::BLUE,
         );
+
+        for (pos, is_wall) in maze.vertical_walls.iter() {
+            if *is_wall {
+                d.draw_line_ex(
+                    Vector2::new(pos.0 as f32 * w as f32, pos.1 as f32 * h as f32),
+                    Vector2::new(pos.0 as f32 * w as f32, pos.1 as f32 * h as f32 + h as f32),
+                    1.0,
+                    Color::BLACK,
+                );
+            }
+        }
+
+        for (pos, is_wall) in maze.horizontal_walls.iter() {
+            if *is_wall {
+                d.draw_line_ex(
+                    Vector2::new(pos.0 as f32 * w as f32, pos.1 as f32 * h as f32),
+                    Vector2::new(pos.0 as f32 * w as f32 + w as f32, pos.1 as f32 * h as f32),
+                    1.0,
+                    Color::BLACK,
+                );
+            }
+        }
     }
 }
--- src/maze.rs
+++ src/maze.rs
@@ -1,36 +1,45 @@
 use rand::{thread_rng, Rng};
+use std::collections::HashMap;
 
 type Position = (i32, i32);
-
-#[derive(Copy, Clone, PartialEq, Debug)]
-pub enum CellType {
-    Wall,
-    Path,
-}
 
 pub struct Cell {
     pub x: i32,
     pub y: i32,
-    pub cell_type: CellType,
+    pub visited: bool
 }
 
 pub struct Maze {
     pub width: i32,
     pub height: i32,
-    pub cells: Vec<Vec<CellType>>,
+    pub cells: Vec<Vec<bool>>,
+    pub horizontal_walls: HashMap<Position, bool>,
+    pub vertical_walls: HashMap<Position, bool>,
     pub active_position: Position,
     stack: Vec<Position>,
 }
 
 impl Maze {
     pub fn new(width: i32, height: i32) -> Self {
-        let cells = vec![vec![CellType::Wall; height as usize]; width as usize];
+        let cells = vec![vec![false; height as usize]; width as usize];
+        let mut horizontal_walls: HashMap<Position, bool> = HashMap::new();
+        let mut vertical_walls: HashMap<Position, bool> = HashMap::new();
+
+        for y in 0..height {
+            for x in 1..width {
+                vertical_walls.insert((x, y), true);
+                horizontal_walls.insert((y, x), true);
+            }
+        }
+
         Maze {
             width,
             height,
             cells,
             active_position: (0, 0),
             stack: Vec::new(),
+            horizontal_walls,
+            vertical_walls,
         }
     }
 
@@ -38,22 +47,22 @@ impl Maze {
         MazeIterator::new(self)
     }
 
-    pub fn get_cell_type(&self, pos: Position) -> CellType {
+    pub fn is_visited(&self, pos: Position) -> bool {
         self.cells[pos.0 as usize][pos.1 as usize]
     }
 
-    pub fn set_cell_type(&mut self, pos: Position, cell_type: CellType) {
-        self.cells[pos.0 as usize][pos.1 as usize] = cell_type;
+    pub fn set_visited(&mut self, pos: Position, visited: bool) {
+        self.cells[pos.0 as usize][pos.1 as usize] = visited;
     }
 
     fn pick_random_direction(&self) -> Position {
         let mut rng = thread_rng();
         let n = rng.gen_range(0..4);
         match n {
-            0 => (2, 0),
-            1 => (0, 2),
-            2 => (-2, 0),
-            3 => (0, -2),
+            0 => (1, 0),
+            1 => (-1, 0),
+            2 => (0, 1),
+            3 => (0, -1),
             _ => panic!("Invalid direction, this is weird and should not happen."),
         }
     }
@@ -63,22 +72,20 @@ impl Maze {
     }
 
     fn is_dead_end(&self, point: Position) -> bool {
-        let up = (point.0, point.1 - 2);
-        let down = (point.0, point.1 + 2);
-        let left = (point.0 - 2, point.1);
-        let right = (point.0 + 2, point.1);
+        let up = (point.0, point.1 - 1);
+        let down = (point.0, point.1 + 1);
+        let left = (point.0 - 1, point.1);
+        let right = (point.0 + 1, point.1);
 
         [up, down, left, right]
             .iter()
             .filter(|p| !self.outside_grid(**p))
-            .all(|p| self.get_cell_type(*p) == CellType::Path)
+            .all(|p| self.is_visited(*p))
     }
 
     fn backtrack(&mut self) {
         if let Some(pos) = self.stack.pop() {
             self.active_position = pos;
-        } else {
-            println!("No more cells to move to.");
         }
     }
 
@@ -93,14 +100,27 @@ impl Maze {
             return;
         } else if self.is_dead_end(self.active_position) {
             self.backtrack();
-        } else if self.get_cell_type(next_cell) == CellType::Wall {
-            let cell_behind_next_cell = (next_cell.0 - dir.0 / 2, next_cell.1 - dir.1 / 2);
-            self.set_cell_type(self.active_position, CellType::Path);
-            self.set_cell_type(cell_behind_next_cell, CellType::Path);
-            self.set_cell_type(next_cell, CellType::Path);
+        } else if !self.is_visited(next_cell) {
+            self.set_visited(self.active_position, true);
+            self.set_visited(next_cell, true);
+
+            match dir {
+                (1, 0) => {
+                    self.vertical_walls.insert(next_cell, false);
+                }
+                (-1, 0) => {
+                    self.vertical_walls.insert(self.active_position, false);
+                }
+                (0, 1) => {
+                    self.horizontal_walls.insert(next_cell, false);
+                }
+                (0, -1) => {
+                    self.horizontal_walls.insert(self.active_position, false);
+                }
+                _ => panic!("Invalid direction, this is weird and should not happen."),
+            }
 
             self.stack.push(self.active_position);
-
             self.active_position = next_cell;
         }
     }
@@ -122,14 +142,14 @@ impl<'a> Iterator for MazeIterator<'a> {
     type Item = Cell;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.x > self.maze.width - 1 || self.y > self.maze.height - 1 {
+        if self.maze.outside_grid((self.x, self.y)) {
             return None;
         }
 
         let return_value = Some(Cell {
             x: self.x,
             y: self.y,
-            cell_type: self.maze.cells[self.x as usize][self.y as usize],
+            visited: self.maze.is_visited((self.x, self.y)),
         });
 
         self.x += 1;
